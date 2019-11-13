bfa914f821585bb8e80567292185b9d850cfc916
Shurkodr <sashaglazunov2@gmail.com>
2019-11-13 16:04:02 +0200

    Updates

---
+++ precache-manifest.09725c88c40b248eef573608dc75c4d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fffe00618e419dfe572974196e371d3",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "02810fd3c55a14f9e73e",
+    "url": "/react_add-todo-form/static/css/main.158ffc0a.chunk.css"
+  },
+  {
+    "revision": "11ff868d0a8a576262f7",
+    "url": "/react_add-todo-form/static/js/2.0612fd57.chunk.js"
+  },
+  {
+    "revision": "02810fd3c55a14f9e73e",
+    "url": "/react_add-todo-form/static/js/main.912b3cdf.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
